db839e195c3f35f06b748b2f2475c5b47f94b30c
webmobman <30666405+webmobman@users.noreply.github.com>
2018-07-30 13:58:14 +0600

    Update clientversion.h

--- src/clientversion.h
+++ src/clientversion.h
@@ -39,7 +39,7 @@
 #define DO_STRINGIZE(X) #X
 
 //! Copyright string used in Windows .rc files
-#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) " The PIVX Core Developers, 2017-" STRINGIZE(COPYRIGHT_YEAR) " The Transcendence Core Developers"
+#define COPYRIGHT_STR "2009-" STRINGIZE(COPYRIGHT_YEAR) " The Bitcoin Core Developers, 2014-" STRINGIZE(COPYRIGHT_YEAR) " The Dash Core Developers, 2015-" STRINGIZE(COPYRIGHT_YEAR) "The PIVX Core Developers, " STRINGIZE(COPYRIGHT_YEAR) " -Hell Freezes The Transcendence Core Developers"
 
 /**
  * transcendenced-res.rc includes this file, but it cannot cope with real c++ code.

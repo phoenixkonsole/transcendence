42810e8ab8001d4d19bd80398035f12df5a37862
webmobman <30666405+webmobman@users.noreply.github.com>
2018-07-30 13:38:37 +0600

    Version Update

--- src/clientversion.h
+++ src/clientversion.h
@@ -16,7 +16,7 @@
 
 //! These need to be macros, as clientversion.cpp's and transcendence*-res.rc's voodoo requires it
 #define CLIENT_VERSION_MAJOR 1
-#define CLIENT_VERSION_MINOR 0
+#define CLIENT_VERSION_MINOR 1
 #define CLIENT_VERSION_REVISION 0
 #define CLIENT_VERSION_BUILD 0
 
@@ -27,7 +27,7 @@
  * Copyright year (2009-this)
  * Todo: update this when changing our copyright comments in the source
  */
-#define COPYRIGHT_YEAR 2020
+#define COPYRIGHT_YEAR 2018
 
 #endif //HAVE_CONFIG_H
 

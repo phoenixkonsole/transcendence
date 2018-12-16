53a767b731b8859aa67288c9aad08abd62a64303
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2018-12-16 11:43:26 +0100

    Update chainparams.cpp
    Changed defaultport to 51476 for mainnet. 51476 is outside the IANA registered Port Range. It is to check if this port is closed by some operating systems !

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -105,7 +105,7 @@ public:
         pchMessageStart[2] = 0x4d;
         pchMessageStart[3] = 0x39;
         vAlertPubKey = ParseHex("0317dc37cb1011c0a162fc113358cdad2a12b85a0f841c4c48346bd85f45ec5c1d");
-        nDefaultPort = 22123;
+        nDefaultPort = 51476;
         bnProofOfWorkLimit = ~uint256(0) >> 20; // Transcendence starting difficulty is 1 / 2^12
         nSubsidyHalvingInterval = 210000;
         nMaxReorganizationDepth = 100;

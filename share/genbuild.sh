4bf8bb5ff8012ba000b881192a1e800b82b2a3c0
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2019-02-07 11:46:54 +0100

    changed revision letter
    We are not going to fork before we setup up new rules regarding rewards and collateral (or decided to keep it as it is)

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -13,9 +13,9 @@ else
     exit 1
 fi
 
-DESC="1.1.0.0c"
+DESC="1.1.0.0d"
 SUFFIX=""
-LAST_COMMIT_DATE="Sun Jan 27 17:06:48 2019 +0200"
+LAST_COMMIT_DATE="Thu Feb 07 11:46:48 2019 +0200"
 if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/dev/null)" = "true" ]; then
     # clean 'dirty' status of touched files that haven't been modified
     git diff >/dev/null 2>/dev/null

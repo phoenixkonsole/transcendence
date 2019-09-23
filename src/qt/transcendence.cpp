8b52bec05691ada4684c78b8401c4cf695d12521
Akshay <akshaycm@hotmail.com>
2019-09-24 00:02:23 +0300

    update cli refernces to transcendence core

--- src/qt/transcendence.cpp
+++ src/qt/transcendence.cpp
@@ -681,7 +681,7 @@ int main(int argc, char* argv[])
         app.createWindow(networkStyle.data());
         app.requestInitialize();
 #if defined(Q_OS_WIN)
-        WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("PIVX Core didn't yet exit safely..."), (HWND)app.getMainWinId());
+        WinShutdownMonitor::registerShutdownBlockReason(QObject::tr("Transcendence Core didn't yet exit safely..."), (HWND)app.getMainWinId());
 #endif
         app.exec();
         app.requestShutdown();
--- src/transcendence-cli.cpp
+++ src/transcendence-cli.cpp
@@ -71,10 +71,10 @@ static bool AppInitRPC(int argc, char* argv[])
     //
     ParseParameters(argc, argv);
     if (argc < 2 || mapArgs.count("-?") || mapArgs.count("-help") || mapArgs.count("-version")) {
-        std::string strUsage = _("PIVX Core RPC client version") + " " + FormatFullVersion() + "\n";
+        std::string strUsage = _("Transcendence Core RPC client version") + " " + FormatFullVersion() + "\n";
         if (!mapArgs.count("-version")) {
             strUsage += "\n" + _("Usage:") + "\n" +
-                        "  transcendence-cli [options] <command> [params]  " + _("Send command to PIVX Core") + "\n" +
+                        "  transcendence-cli [options] <command> [params]  " + _("Send command to Transcendence Core") + "\n" +
                         "  transcendence-cli [options] help                " + _("List commands") + "\n" +
                         "  transcendence-cli [options] help <command>      " + _("Get help for a command") + "\n";
 

3cfd74def0d8ea243a3f7097350e9848cebfe324
Akshay <akshaycm@hotmail.com>
2019-09-24 02:54:56 +0300

    remove marginx

--- src/qt/transcendence/masternodeswidget.cpp
+++ src/qt/transcendence/masternodeswidget.cpp
@@ -236,10 +236,9 @@ void MasterNodesWidget::initChart() {
     chart->legend()->setAlignment(Qt::AlignTop);
     chart->setBackgroundBrush(QBrush(backgroundColor));
     chart->setAnimationOptions(QChart::SeriesAnimations);
-    chart->layout()->setContentsMargins(0, 0, 0, 0);
     chart->setMargins({0, 0, 0, 0});
     chart->setBackgroundRoundness(0);
-    
+
     chartView = new QChartView(chart);
     chartView->setRenderHint(QPainter::Antialiasing);
     chartView->setContentsMargins(0,0,0,0);

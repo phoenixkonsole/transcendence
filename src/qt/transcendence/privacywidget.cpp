3530f61847268db41b6102ee25db9691e9e95603
Akshay <akshaycm@hotmail.com>
2019-10-07 22:02:16 +0530

    update incluide

--- src/qt/transcendence/privacywidget.cpp
+++ src/qt/transcendence/privacywidget.cpp
@@ -3,7 +3,7 @@
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include "qt/transcendence/privacywidget.h"
-#include "qt/transcendence/settings/forms/ui_privacywidget.h"
+#include "qt/transcendence/forms/ui_privacywidget.h"
 #include <QScrollBar>
 #include <QMetaObject>
 #include "qt/transcendence/qtutils.h"

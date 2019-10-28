5a5761df1c3b2a3e905dd3e55eac93a335bdb9b2
Akshay <akshaycm@hotmail.com>
2019-10-28 09:44:45 +0530

    more zerocoin mint disabling

--- src/util.cpp
+++ src/util.cpp
@@ -115,9 +115,9 @@ bool fLiteMode = false;
 bool fEnableSwiftTX = true;
 int nSwiftTXDepth = 5;
 // Automatic Zerocoin minting
-bool fEnableZeromint = true;
+bool fEnableZeromint = false;
 bool fEnableAutoConvert = false;
-int nZeromintPercentage = 10;
+int nZeromintPercentage = 0;
 int nPreferredDenom = 0;
 const int64_t AUTOMINT_DELAY = (60 * 5); // Wait at least 5 minutes until Automint starts
 

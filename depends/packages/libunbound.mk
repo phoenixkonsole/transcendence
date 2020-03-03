4723f024a45c18ca8ec6f90446304d3cf5d9cf52
DancingAxolotl <5020980+DancingAxolotl@users.noreply.github.com>
2020-03-03 17:52:20 +0200

    Fix libunbound

--- depends/packages/libunbound.mk
+++ depends/packages/libunbound.mk
@@ -1,8 +1,8 @@
 package=libunbound
-$(package)_version=1.9.6
+$(package)_version=1.10.0
 $(package)_download_path=https://github.com/NLnetLabs/unbound/archive/
 $(package)_file_name=release-$($(package)_version).tar.gz
-$(package)_sha256_hash=12e85421d49564d17918f5430b46787a7ddff36c7b25cfa7538836cf761d8f72
+$(package)_sha256_hash=F6107A5019EC920F827603AD51550D7024FD03B8274582945ADDAB4A95A90579
 $(package)_dependencies=openssl expat
 
 define $(package)_config_cmds
@@ -15,4 +15,8 @@ endef
 
 define $(package)_stage_cmds
   $(MAKE) DESTDIR=$($(package)_staging_dir) install
+endef
+
+define $(package)_set_vars
+  $(package)_config_opts=--disable-shared --enable-static-exe
 endef

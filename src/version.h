42ad6c11296803eea326ff81cd044219483cd9f5
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2020-07-04 20:49:47 +0200

    Update version.h
    enforce disconnect peers older than 71005

--- src/version.h
+++ src/version.h
@@ -21,8 +21,8 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71002;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71002;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71005;
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71005;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this

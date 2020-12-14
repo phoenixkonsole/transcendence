a4d17b288d8c679fa1ab681d16bd89d8e7220ca7
RSerhii <shraam@ukr.net>
2020-12-14 17:02:49 +0200

    Fork on 2020-12-16 21:30 UTC

--- src/version.h
+++ src/version.h
@@ -23,6 +23,7 @@ static const int GETHEADERS_VERSION = 70000;
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71006;
 static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71006;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_FORK = 71005; 
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this

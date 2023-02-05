6e9361fa484ff50324a2a5b30d017726141c6c98
phoenixkonsole <40170965+phoenixkonsole@users.noreply.github.com>
2023-02-05 18:22:38 +0100

    Update version.h

--- src/version.h
+++ src/version.h
@@ -22,8 +22,8 @@ static const int GETHEADERS_VERSION = 70000;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION_BEFORE_ENFORCEMENT = 71005;
-static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71006;
-static const int MIN_PEER_PROTO_VERSION_BEFORE_FORK = 71005; 
+static const int MIN_PEER_PROTO_VERSION_AFTER_ENFORCEMENT = 71007;
+static const int MIN_PEER_PROTO_VERSION_BEFORE_FORK = 71006; 
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
